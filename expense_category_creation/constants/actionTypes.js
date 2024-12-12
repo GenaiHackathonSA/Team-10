4c3fe5d883af66a31dfe824805ae7ab73b75e3da
GenaiHackathonSA <genaihackathonsa@gmail.com>
2024-12-12 16:11:22 +0530

    feat: commit for T10-68

---
+++ expense_category_creation/constants/actionTypes.js
@@ -0,0 +1,4 @@
+// expense_category_creation/constants/actionTypes.js
+
+// Action types for expense category related actions
+export const ADD_EXPENSE_CATEGORY = 'ADD_EXPENSE_CATEGORY'; // This constant will represent the action type for adding a new expense category in the Redux store.
